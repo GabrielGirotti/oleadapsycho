5e7444f831fc7f1aed196810254840caca971693
GabrielGirotti <girottigabriel@gmail.com>
2024-12-03 07:41:44 -0300

    png to avif

--- src/components/CarouselLineUp.tsx
+++ src/components/CarouselLineUp.tsx
@@ -5,71 +5,71 @@ export default function CarouselLineUp() {
   return (
     <Carousel infiniteLoop={true} className=" w-[90vw] max-w-[600px]">
       <div>
-        <img src="/lineup/z.png" alt="Angry Zeta" />
+        <img src="/lineup/z.avif" alt="Angry Zeta" />
       </div>
       <div>
-        <img src="/lineup/ratas.png" alt="Las Ratas Sicodelicas" />
+        <img src="/lineup/ratas.avif" alt="Las Ratas Sicodelicas" />
       </div>
       <div>
-        <img src="/lineup/jf.png" alt="Jinetes Fantasmas" />
+        <img src="/lineup/jf.avif" alt="Jinetes Fantasmas" />
       </div>
       <div>
-        <img src="/lineup/fish.png" alt="Fish'n Creepers" />
+        <img src="/lineup/fish.avif" alt="Fish'n Creepers" />
       </div>
       <div>
-        <img src="/lineup/gb.png" alt="Ghost Bastards" />
+        <img src="/lineup/gb.avif" alt="Ghost Bastards" />
       </div>
       <div>
-        <img src="/lineup/cardiax.png" alt="Cardiax" />
+        <img src="/lineup/cardiax.avif" alt="Cardiax" />
       </div>
       <div>
-        <img src="/lineup/marcos.png" alt="Marcos Ferragut" />
+        <img src="/lineup/marcos.avif" alt="Marcos Ferragut" />
       </div>
       <div>
-        <img src="/lineup/horror.png" alt="Horror Ink" />
+        <img src="/lineup/horror.avif" alt="Horror Ink" />
       </div>
       <div>
-        <img src="/lineup/clan.png" alt="Clan Farsante" />
+        <img src="/lineup/clan.avif" alt="Clan Farsante" />
       </div>
       <div>
-        <img src="/lineup/zorros.png" alt="Zorros Petardos Salvajes" />
+        <img src="/lineup/zorros.avif" alt="Zorros Petardos Salvajes" />
       </div>
       <div>
-        <img src="/lineup/peyo.png" alt="Elvis Peyo Barrios" />
+        <img src="/lineup/peyo.avif" alt="Elvis Peyo Barrios" />
       </div>
       <div>
-        <img src="/lineup/mancos.png" alt="Mancos" />
+        <img src="/lineup/mancos.avif" alt="Mancos" />
       </div>
 
       <div>
-        <img src="/lineup/sickbay.png" alt="Sick Bay" />
+        <img src="/lineup/sickbay.avif" alt="Sick Bay" />
       </div>
       <div>
-        <img src="/lineup/nervios.png" alt="Nervios Calavera" />
+        <img src="/lineup/nervios.avif" alt="Nervios Calavera" />
       </div>
       <div>
-        <img src="/lineup/connie.png" alt="Connie Darko" />
+        <img src="/lineup/connie.avif" alt="Connie Darko" />
       </div>
       <div>
-        <img src="/lineup/nasty.png" alt="Nasty Neighbours" />
+        <img src="/lineup/nasty.avif" alt="Nasty Neighbours" />
       </div>
       <div>
-        <img src="/lineup/criadores.png" alt="Criadores de Odio" />
+        <img src="/lineup/criadores.avif" alt="Criadores de Odio" />
       </div>
       <div>
-        <img src="/lineup/nickenson.png" alt="Nickenson" />
+        <img src="/lineup/nickenson.avif" alt="Nickenson" />
       </div>
       <div>
-        <img src="/lineup/tequila.png" alt="Tequila Pontiac" />
+        <img src="/lineup/tequila.avif" alt="Tequila Pontiac" />
       </div>
       <div>
-        <img src="/lineup/bitter.png" alt="Bitter Shoes" />
+        <img src="/lineup/bitter.avif" alt="Bitter Shoes" />
       </div>
       <div>
-        <img src="/lineup/links.png" alt="Los Links" />
+        <img src="/lineup/links.avif" alt="Los Links" />
       </div>
       <div>
-        <img src="/lineup/dias.png" alt="Dias de Noche" />
+        <img src="/lineup/dias.avif" alt="Dias de Noche" />
       </div>
     </Carousel>
   );
--- src/components/Design.tsx
+++ src/components/Design.tsx
@@ -1,8 +1,8 @@
 export default function Design() {
   return (
-    <div className="bg-[url('/design/bg-design.png')] w-[1280px] h-[720px]">
+    <div className="bg-[url('/design/bg-design.avif')] w-[1280px] h-[720px]">
       <img
-        src="/design/design.png"
+        src="/design/design.avif"
         alt="Frame"
         className="absolute top-0 hidden md:block"
       />
